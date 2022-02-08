f4688302ec78c0cabd98f5868059e67115bfd200
Nikola Lazarov <94927697+NikolaLazarov@users.noreply.github.com>
2022-02-08 21:41:42 +0200

    Add files via upload

---
+++ Листинг 8.1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using static System.Console;
+
+namespace Листинг_8._1
+{
+    class myClass
+    {
+        public int numbers;
+        public myClass(int n)
+        {
+            numbers = n;
+        }
+        public static int operator +(myClass a, myClass b)
+        {
+            int m = a.numbers + b.numbers;
+            return m;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            myClass A = new myClass(100);
+            myClass B = new myClass(200);
+
+            int num;
+            num = A + B;
+            WriteLine("A + B = {0}", num);
+
+            WriteLine("Press any key to close...");
+            ReadKey(true);
+        }
+    }
+}
