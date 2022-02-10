1b8201942e7883bc74b5d004fb50930a44276f2f
Nikola Lazarov <94927697+NikolaLazarov@users.noreply.github.com>
2022-02-10 22:50:39 +0200

    Add files via upload

---
+++ Листинг 8.6/Program.cs
@@ -0,0 +1,63 @@
+﻿using static System.Console;
+//creating a new class
+class Alpha{
+    //creating a new number variable
+    public int code;
+    //creating a constructor with one argument
+    public Alpha(int n){
+        //assigning the original variables value to arguments
+        code = n;
+    }
+}
+//creating a new class
+class Bravo{
+    //creating a new number variable
+    public int code;
+    //creating a constructor with one argument
+    public Bravo(int n){
+        //assigning the original variables value to arguments
+        code = n;
+    }
+    //creating a new constructor with two arguments
+    //both being a instance of Bravo
+    public static bool operator ==(Bravo a, Bravo b){
+        //return the result of the comparison
+        return a.code == b.code;
+    }
+    //creating a new constructor with two arguments
+    //both being a instance of Bravo
+    public static bool operator !=(Bravo a, Bravo b){
+        //return the opposite result of the comparison
+        return !(a == b);
+    }
+}
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        //creating 3 objects
+        Alpha A1 = new Alpha(100);
+        Alpha A2 = new Alpha(100);
+        Alpha A3 = new Alpha(200);
+
+        //displaying the comparison between the objects
+        WriteLine("A1==A2 дава {0}",A1==A2);
+        WriteLine("A1!=A2 дава {0}",A1!=A2);
+        WriteLine("A1==A3 дава {0}",A1==A3);
+        WriteLine("A1!=A3 дава {0}",A1!=A3);
+
+        //creating 3 objects
+        Bravo B1 =  new Bravo(100);
+        Bravo B2 = new Bravo(100);
+        Bravo B3 = new Bravo(200);
+
+        //displaying the comparison between the objects
+        WriteLine("B1==B2 дава {0}",B1==B2);
+        WriteLine("B1!=B2 дава {0}",B1!=B2);
+        WriteLine("B1==B3 дава {0}",B1==B3);
+        WriteLine("B1!=B3 дава {0}",B1!=B3);
+
+        WriteLine("Press any key to close...");
+        ReadKey(true);
+    }
+}
---
+++ Листинг 8.7/Program.cs
@@ -0,0 +1,77 @@
+﻿using static System.Console;
+//creating new class
+class myClass
+{
+    //creating a number variable
+    public int code;
+    //creating a character variable
+    public char symb;
+    //creating a constructor with two arguments
+    //a number one and a character one
+    public myClass(int n, char s)
+    {
+        //assigning the original variables values to the arguments
+        code = n;
+        symb = s;
+    }
+    //pre-defining a GetHashCode method
+    public override int GetHashCode()
+    {
+        //return the result of calculation
+        return code ^ symb;
+    }
+    //pre-defining an Equals method with a object as argument
+    public override bool Equals(Object obj)
+    {
+        //creating a new class variable
+        //assinging its value to the arguments value as a references to myClass
+        myClass t = (myClass)obj;
+        //the original variables values are the same as the new ones 
+        //return true
+        if (code == t.code && symb == t.symb) return true;
+        //else return false
+        else return false;
+    }
+    //creating a operator with two object as arguments
+    public static bool operator ==(myClass a, myClass b)
+    {
+        //return the result of the method equals
+        //using Equals() on a with b as reference
+        return a.Equals(b);
+    }
+    //creating a operator with two object as arguments
+    public static bool operator !=(myClass a, myClass b)
+    {
+        //returns the opposite of the == operator
+        return !(a == b);
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        //creating object of myClass
+        myClass A = new myClass(100, 'A');
+        myClass B = new myClass(100, 'B');
+        myClass C = new myClass(200, 'C');
+        myClass D = new myClass(100, 'A');
+
+        //displaying the HashCode of the objects
+        WriteLine("Хеш-код A: {0}", A.GetHashCode());
+        WriteLine("Хеш-код B: {0}", B.GetHashCode());
+        WriteLine("Хеш-код C: {0}", C.GetHashCode());
+        WriteLine("Хеш-код D: {0}", D.GetHashCode());
+
+        //displaying the comparison between the objects
+        WriteLine("A==B дава {0}", A == B);
+        WriteLine("A!=B дава {0}", A != B);
+        WriteLine("A==C дава {0}", A == C);
+        WriteLine("A!=C дава {0}", A != C);
+        WriteLine("A==D дава {0}", A == D);
+        WriteLine("A!=D дава {0}", A != D);
+
+        WriteLine("Press any key to close...");
+        ReadKey(true);
+    }
+}
