cfc5fe6cbcce9fa03e4e1899568fbf372f708161
Nikola Lazarov <94927697+NikolaLazarov@users.noreply.github.com>
2022-02-11 13:39:13 +0200

    Add files via upload

---
+++ Листинг 8.8/Program.cs
@@ -0,0 +1,54 @@
+﻿using static System.Console;
+//creating new class
+class myClass
+{
+    //creating new number class variable
+    public int code;
+    //creating constructor with one argument
+    public myClass(int n)
+    {
+        //assigning the number variable to the argument
+        code = n;
+    }
+    //creating a operator with a class object as argument
+    public static bool operator true(myClass obj)
+    {
+        //if the objects number field is higher or equal to 5
+        //the operator returns true
+        if (obj.code >= 5) return true;
+        //else the operator returns false
+        else return false;
+    }
+    //creating a operator with a class object as argument
+    public static bool operator false(myClass obj)
+    {
+        //if the operator "true"s value is true
+        //the operator returns false
+        if (obj) return false;
+        //else the operator returns true
+        return true;
+    }
+}
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        //creating a new object
+        myClass obj = new myClass(10);
+        //creating a loop
+        //while the operator "true" returns true
+        //execute the code in the loop
+        while (obj)
+        {
+            //displaying the objects number field plus a blank space
+            Write(obj.code + " ");
+            //subtracts one from the number field
+            obj.code--;
+        }
+        //displaying new line
+        WriteLine();
+
+        WriteLine("Press any key to close...");
+        ReadKey(true);
+    }
+}
---
+++ Листинг 8.9/Program.cs
@@ -0,0 +1,94 @@
+﻿using static System.Console;
+//creating new class
+class myClass
+{
+    //creating new class character variable
+    public char symb;
+    //creating constructor with character variable as argument
+    public myClass(char s)
+    {
+        //assigning the variables values to the arguments
+        symb = s;
+    }
+    //creating new operator with one object as argument
+    public static bool operator true(myClass obj)
+    {
+        //checks if the objects char fields are equal to one of the cases
+        switch (obj.symb)
+        {
+            case 'A':
+            case 'E':
+            case 'I':
+            case 'O':
+            case 'U':
+            case 'Y':
+            //if it is return true
+                return true;
+            default:
+            //if not return false
+                return false;
+        }
+    }
+    //creating new operator with object as argument
+    public static bool operator false(myClass obj)
+    {
+        //if the statement is true,return the objects character filed as "Y";
+        if (obj) return obj.symb == 'Y';
+        //else return false
+        else return false;
+    }
+    //creating a new operator with 2 objects as arguments
+    public static myClass operator &(myClass a, myClass b)
+    {
+        //if the first object is true
+        //return the second object
+        if (a) return b;
+        //else return the first one
+        else return a;
+    }
+    //creating new object with 2 objects as arguments
+    public static myClass operator |(myClass a, myClass b)
+    {
+        //if the first object is true
+        //return the first object
+        if (a) return a;
+        //else return the second one
+        else return b;
+    }
+}
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        //creating 4 objects
+        myClass A = new myClass('A');
+        myClass B = new myClass('B');
+        myClass E = new myClass('E');
+        myClass Y = new myClass('Y');
+
+        //displaying the comparisons between them
+        //(A&&B)-tests A to see if its a lie
+        //if false execute the operation A&B
+        //if true return A
+        WriteLine("Израз А&&B: {0}", (A && B).symb);
+        WriteLine("Израз B&&A: {0}", (B && A).symb);
+        WriteLine("Израз A&&E: {0}", (A && E).symb);
+        WriteLine("Израз E&&A: {0}", (E && A).symb);
+        WriteLine("Израз A&&Y: {0}", (A && Y).symb);
+        WriteLine("Израз Y&&A: {0}", (Y && A).symb);
+
+        //(A||B)-test to see if its a lie
+        //if false execute the operation A|B
+        //if true return A
+        WriteLine("Израз A||B: {0}", (A || B).symb);
+        WriteLine("Израз B||A: {0}", (B || A).symb);
+        WriteLine("Израз A||E: {0}", (A || E).symb);
+        WriteLine("Израз E||A: {0}", (E || A).symb);
+        WriteLine("Израз A||Y: {0}", (A || Y).symb);
+        WriteLine("Израз Y||A: {0}", (Y || A).symb);
+
+
+        WriteLine("Press any key to close the");
+        ReadKey(true);
+    }
+}
