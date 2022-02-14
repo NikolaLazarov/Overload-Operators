0208e9a749599f800e97307d0d30559f03d4fc7e
Nikola Lazarov <94927697+NikolaLazarov@users.noreply.github.com>
2022-02-14 22:52:49 +0200

    Add files via upload

---
+++ Самостоятелни задачи/Задача 1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using static System.Console;
+
+namespace Задача_1
+{
+    class myClass
+    {
+        public char symbol;
+        public myClass(char symbol)
+        {
+            this.symbol = symbol;
+        }
+        public override string ToString()
+        {
+            string text = "The character: " + symbol;
+            return text;
+        }
+        public static myClass operator ++(myClass a)
+        {
+            a.symbol++;
+            return a;
+        }
+        public static myClass operator --(myClass a)
+        {
+            a.symbol--;
+            return a;
+        }
+        public static myClass operator +(myClass a, int b)
+        {
+            int n = (int)a.symbol + b;
+            return new myClass((char)n);
+        }
+        public static int operator +(int b, myClass a)
+        {
+            return (int)a.symbol + b;
+        }
+        public static myClass operator -(myClass a, int b)
+        {
+            int n = (int)a.symbol - b;
+            return new myClass((char)n);
+        }
+        public static int operator -(int b, myClass a)
+        {
+            return (int)a.symbol - b;
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                myClass A = new myClass('G');
+
+                WriteLine(A++);
+                WriteLine(A--);
+                WriteLine(A + 2);
+                WriteLine(A - 2);
+                WriteLine(2 + A);
+                WriteLine(2 - A);
+
+                WriteLine("Press any key to class...");
+                ReadKey(true);
+            }
+        }
+    }
+}
---
+++ Самостоятелни задачи/Задача 2/Program.cs
@@ -0,0 +1,69 @@
+﻿using static System.Console;
+class myClass
+{
+    public int lht;
+    public int[] arrNums = new int[0];
+    public myClass(int a)
+    {
+        lht = a;
+        Array.Resize(ref arrNums, a);
+    }
+    int[] Nums = new int[0];
+    public myClass(int[] arr)
+    {
+        Array.Resize(ref Nums, arr.Length);
+        for (int i = 0; i < Nums.Length; i++)
+        {
+            Nums[i] = arr[i];
+        }
+    }
+    public override string ToString()
+    {
+        string arr = "";
+        foreach (int i in Nums)
+        {
+            arr += i + " ";
+        }
+        return arr;
+    }
+    public static myClass operator ++(myClass a)
+    {
+        return new myClass(a.lht++);
+    }
+    public static myClass operator --(myClass a)
+    {
+        return new myClass(a.lht--);
+    }
+    public static string operator ~(myClass a)
+    {
+        string arr = "";
+        foreach (int i in a.arrNums)
+        {
+            arr += i + " ";
+        }
+        return arr;
+    }
+    public static myClass operator +(myClass a, myClass b)
+    {
+        if (a.lht > b.lht)
+        {
+            myClass jh = new myClass(a.lht);
+            Array.Resize(ref b.arrNums, a.lht);
+            for (int i = 0; i < a.arrNums.Length; i++)
+            {
+                jh.arrNums[i] = a.arrNums[i] + b.arrNums[i];
+                return new myClass(jh.arrNums);
+            }
+        }
+    }
+}
+public class Program
+{
+    public static void Main(string[] args)
+    {
+
+
+        WriteLine("Press any key to close...");
+        ReadKey(true);
+    }
+}
