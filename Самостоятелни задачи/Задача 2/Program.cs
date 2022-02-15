9a6c8ffa43f0c2249ca93c488d7514c66ac97d60
Nikola Lazarov <94927697+NikolaLazarov@users.noreply.github.com>
2022-02-15 19:34:07 +0200

    Update Program.cs

--- Самостоятелни задачи/Задача 2/Program.cs
+++ Самостоятелни задачи/Задача 2/Program.cs
@@ -1,12 +1,16 @@
-﻿using static System.Console;
+using static System.Console;
 class myClass
 {
-    public int lht;
+    public int number;
     public int[] arrNums = new int[0];
     public myClass(int a)
     {
-        lht = a;
+        number = a;
         Array.Resize(ref arrNums, a);
+        for (int i = 0; i < arrNums.Length; i++)
+        {
+            arrNums[i] = 0;
+        }
     }
     int[] Nums = new int[0];
     public myClass(int[] arr)
@@ -20,48 +24,72 @@ class myClass
     public override string ToString()
     {
         string arr = "";
-        foreach (int i in Nums)
+        foreach (int i in arrNums)
         {
-            arr += i + " ";
+            arr += i;
         }
         return arr;
     }
     public static myClass operator ++(myClass a)
     {
-        return new myClass(a.lht++);
+        return new myClass(a.number + 1);
     }
     public static myClass operator --(myClass a)
     {
-        return new myClass(a.lht--);
+        return new myClass(a.number - 1);
     }
     public static string operator ~(myClass a)
     {
-        string arr = "";
-        foreach (int i in a.arrNums)
+        string s = "";
+        foreach (int i in a.Nums)
         {
-            arr += i + " ";
+            s += i;
         }
-        return arr;
+        return s;
+    }
+    public static myClass operator +(myClass a, int b)
+    {
+        Array.Resize(ref a.arrNums, a.arrNums.Length + 1);
+        a.arrNums[a.arrNums.Length - 1] = b;
+        return new myClass(a.arrNums);
+    }
+    public static myClass operator +(int a, myClass b)
+    {
+        Array.Resize(ref b.arrNums, b.arrNums.Length + 1);
+        b.arrNums[0] = a;
+        return new myClass(b.arrNums);
     }
     public static myClass operator +(myClass a, myClass b)
     {
-        if (a.lht > b.lht)
+        myClass jh = new myClass(a.number);
+        if (a.number > b.number)
         {
-            myClass jh = new myClass(a.lht);
-            Array.Resize(ref b.arrNums, a.lht);
+            Array.Resize(ref b.arrNums, a.number);
             for (int i = 0; i < a.arrNums.Length; i++)
             {
                 jh.arrNums[i] = a.arrNums[i] + b.arrNums[i];
-                return new myClass(jh.arrNums);
             }
         }
+        return new myClass(jh.arrNums);
     }
 }
 public class Program
 {
     public static void Main(string[] args)
     {
+        myClass A = new myClass(4);
+        myClass B = new myClass(3);
 
+        A++;
+        WriteLine(A);
+
+        B--;
+        WriteLine(B);
+
+        WriteLine(~(A + B));
+
+        WriteLine(~(A + 3));
+        WriteLine(~(3 + B));
 
         WriteLine("Press any key to close...");
         ReadKey(true);
