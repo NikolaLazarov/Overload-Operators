71161a9361719aaee04b265a13a10ce16e56cdeb
Nikola Lazarov <94927697+NikolaLazarov@users.noreply.github.com>
2022-02-15 22:58:05 +0200

    Add files via upload

---
+++ Самостоятелни задачи/Задача 4/Program.cs
@@ -0,0 +1,31 @@
+﻿using static System.Console;
+class myClass
+{
+    public int num;
+    public string txt = "";
+    public myClass(int a, string b)
+    {
+        num = a;
+        txt = b;
+    }
+    public override int GetHashCode()
+    {
+        return num;
+    }
+    public override bool Equals(Object a)
+    {
+        myClass y1 = (myClass)a;
+        if (num == y1.num && txt == y1.txt) return true;
+        else return false;
+    }
+
+}
+public class Program
+{
+    public static void Main(string[] args)
+    {
+
+        WriteLine("Press any key to close...");
+        ReadKey(true);
+    }
+}
