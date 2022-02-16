003eccf6c97904863bcd80d25ed8aff819e6740f
Nikola Lazarov <94927697+NikolaLazarov@users.noreply.github.com>
2022-02-16 17:57:01 +0200

    Update Program.cs

--- Самостоятелни задачи/Задача 4/Program.cs
+++ Самостоятелни задачи/Задача 4/Program.cs
@@ -1,4 +1,4 @@
-﻿using static System.Console;
+using static System.Console;
 class myClass
 {
     public int num;
@@ -10,7 +10,7 @@ class myClass
     }
     public override int GetHashCode()
     {
-        return num;
+        return base.GetHashCode();
     }
     public override bool Equals(Object a)
     {
@@ -18,12 +18,52 @@ class myClass
         if (num == y1.num && txt == y1.txt) return true;
         else return false;
     }
-
+    public static bool operator ==(myClass a, myClass b)
+    {
+        return a.Equals(b);
+    }
+    public static bool operator !=(myClass a, myClass b)
+    {
+        return !a.Equals(b);
+    }
+    public static bool operator >(myClass a, myClass b)
+    {
+        if (a.txt.Length > b.txt.Length) return true;
+        else return false;
+    }
+    public static bool operator <(myClass a, myClass b)
+    {
+        if (a.txt.Length < b.txt.Length) return true;
+        else return false;
+    }
+    public static bool operator >=(myClass a, myClass b)
+    {
+        if (a.num >= b.num) return true;
+        else return false;
+    }
+    public static bool operator <=(myClass a, myClass b)
+    {
+        if (a.num <= b.num) return true;
+        else return false;
+    }
 }
 public class Program
 {
     public static void Main(string[] args)
     {
+        myClass A = new myClass(7, "Motorcycle");
+        myClass B = new myClass(3, "Motorbike");
+
+        WriteLine("A==B: {0}", A == B);
+        WriteLine("A!=B: {0}", A != B);
+        WriteLine("A>B: {0}", A > B);
+        WriteLine("A<B: {0}", A < B);
+        WriteLine("A>=B: {0}", A >= B);
+        WriteLine("A<=B: {0}", A <= B);
+
+        WriteLine(A.GetHashCode());
+        WriteLine(B.GetHashCode());
+
 
         WriteLine("Press any key to close...");
         ReadKey(true);
