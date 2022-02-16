c916f5c76049affeebaa21144c56e01f65dd9597
Nikola Lazarov <94927697+NikolaLazarov@users.noreply.github.com>
2022-02-16 17:56:13 +0200

    Add files via upload

---
+++ Самостоятелни задачи/Задача 5/Program.cs
@@ -0,0 +1,46 @@
+﻿using static System.Console;
+class myClass
+{
+    public int num;
+    public char symb;
+    public myClass(int a, char b)
+    {
+        num = a;
+        symb = b;
+    }
+    public static bool operator true(myClass a)
+    {
+        if (a.num > (int)a.symb)
+        {
+            if (a.num - (int)a.symb > 10) return true;
+            else return false;
+        }
+        else
+        {
+            if ((int)a.symb - a.num > 10) return true;
+            else return false;
+        }
+    }
+    public static bool operator false(myClass a)
+    {
+        if (a) return false;
+        else return true;
+    }
+}
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        myClass A = new myClass(60, 'A');
+        myClass B = new myClass(70, 'w');
+
+        if (A) { WriteLine(true); }
+        else { WriteLine(false); }
+
+        if (B) { WriteLine(true); }
+        else { WriteLine(false); }
+
+        WriteLine("Press any key to close...");
+        ReadKey(true);
+    }
+}
