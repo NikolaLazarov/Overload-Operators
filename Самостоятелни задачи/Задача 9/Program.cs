a4daacf74b2869d8cdc460d4b8b838ea158ba5a4
Nikola Lazarov <94927697+NikolaLazarov@users.noreply.github.com>
2022-02-17 19:13:30 +0200

    Add files via upload

---
+++ Самостоятелни задачи/Задача 9/Program.cs
@@ -0,0 +1,39 @@
+﻿using static System.Console;
+class myClass
+{
+    public int numb;
+    public myClass(int a)
+    {
+        numb = a;
+    }
+    public static int operator +(myClass a, myClass b)
+    {
+        return a.numb += b.numb;
+    }
+    public static int operator -(myClass a, myClass b)
+    {
+        return a.numb -= b.numb;
+    }
+    public static int operator *(myClass a, myClass b)
+    {
+        return a.numb *= b.numb;
+    }
+}
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        myClass A = new myClass(9);
+        myClass B = new myClass(7);
+
+        myClass a = new myClass(A + B);
+        WriteLine("A + B = {0}", a.numb);
+        myClass b = new myClass(A - B);
+        WriteLine("A - B = {0}", b.numb);
+        myClass c = new myClass(A * B);
+        WriteLine("A * B = {0}", c.numb);
+
+        WriteLine("Press any key to close...");
+        ReadKey(true);
+    }
+}
