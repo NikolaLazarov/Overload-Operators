a1df94347266264ceda71d731f4ca6fc5c7183c0
Nikola Lazarov <94927697+NikolaLazarov@users.noreply.github.com>
2022-02-17 19:28:05 +0200

    Add files via upload

---
+++ Самостоятелни задачи/Задача 10/Program.cs
@@ -0,0 +1,35 @@
+﻿using static System.Console;
+class myClass
+{
+    public char symb;
+    public myClass(char a)
+    {
+        symb = a;
+    }
+    public static String operator +(myClass a, myClass b)
+    {
+        String str = "";
+        str += a.symb;
+        str += b.symb;
+        return str;
+    }
+    public static int operator -(myClass a, myClass b)
+    {
+        int n = (int)a.symb - (int)b.symb;
+        return n;
+    }
+}
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        myClass A = new myClass('H');
+        myClass B = new myClass('Q');
+
+        WriteLine("A + B = {0}", A + B);
+        WriteLine("A - B = {0}", A - B);
+
+        WriteLine("Press any key to close...");
+        ReadKey(true);
+    }
+}
